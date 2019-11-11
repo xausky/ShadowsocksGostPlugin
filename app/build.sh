f9a8c24b07ec3b93b6f4260361652c77975627a2
xausky <master@xausky.cn>
2019-11-11 11:27:01 +0800

    更新 Action

--- app/build.sh
+++ app/build.sh
@@ -1,16 +1,27 @@
 cd $( cd "$( dirname "$0"  )" && pwd  )
+
+go version
+
+mkdir build
+
+cd build
+
+git clone https://github.com/xausky/gost.git
+
+cd gost
+
 CC=$(find $ANDROID_NDK_ROOT | grep 'armv7a-linux-androideabi21-clang$') \
 GOOS="android" GOARCH="arm" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../jniLibs/armeabi-v7a/libgost-plugin.so github.com/ginuerzh/gost/cmd/gost
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'aarch64-linux-android21-clang$') \
 GOOS="android" GOARCH="arm64" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../jniLibs/arm64-v8a/libgost-plugin.so github.com/ginuerzh/gost/cmd/gost
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'i686-linux-android21-clang$') \
 GOOS="android" GOARCH="386" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../jniLibs/x86/libgost-plugin.so github.com/ginuerzh/gost/cmd/gost
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'x86_64-linux-android21-clang$') \
 GOOS="android" GOARCH="amd64" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../jniLibs/x86_64/libgost-plugin.so github.com/ginuerzh/gost/cmd/gost
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
--- app/src/main/go/go.mod
+++
@@ -1,7 +0,0 @@
-module gost-ss-plugin
-
-go 1.13
-
-replace github.com/ginuerzh/gost => github.com/xausky/gost v2.6.1+incompatible
-
-require github.com/ginuerzh/gost v2.6.1+incompatible // indirect
