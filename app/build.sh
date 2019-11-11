930b7f839e844a744b02f01ca57d9e540f9d29e9
xausky <master@xausky.cn>
2019-11-11 11:36:13 +0800

    更新 build.sh

--- app/build.sh
+++ app/build.sh
@@ -12,16 +12,16 @@ cd gost
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'armv7a-linux-androideabi21-clang$') \
 GOOS="android" GOARCH="arm" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so ./cmd/gost
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'aarch64-linux-android21-clang$') \
 GOOS="android" GOARCH="arm64" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/arm64-v8a/libgost-plugin.so ./cmd/gost
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'i686-linux-android21-clang$') \
 GOOS="android" GOARCH="386" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/x86/libgost-plugin.so ./cmd/gost
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'x86_64-linux-android21-clang$') \
 GOOS="android" GOARCH="amd64" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/armeabi-v7a/libgost-plugin.so .
+go build -ldflags "-s -w" -a -o ../../src/main/jniLibs/x86_64/libgost-plugin.so ./cmd/gost
