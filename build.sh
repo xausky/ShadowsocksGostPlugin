b28832baf4879f5ec463686956af9d075b47cf68
segfault-bilibili <segfault_bilibili@qq.com>
2022-10-25 01:38:54 +0800

    lazy fix for error obtaining VCS status

--- build.sh
+++ build.sh
@@ -44,16 +44,16 @@ echo "ANDROID_NDK_ROOT=$ANDROID_NDK_ROOT"
 cd gost
 CC=$(find $ANDROID_NDK_ROOT | grep 'armv7a-linux-androideabi21-clang$') \
 GOOS="android" GOARCH="arm" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/armeabi-v7a/libgost-plugin.so ./cmd/gost
+go build -buildvcs=false -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/armeabi-v7a/libgost-plugin.so ./cmd/gost
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'aarch64-linux-android21-clang$') \
 GOOS="android" GOARCH="arm64" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/arm64-v8a/libgost-plugin.so ./cmd/gost
+go build -buildvcs=false -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/arm64-v8a/libgost-plugin.so ./cmd/gost
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'i686-linux-android21-clang$') \
 GOOS="android" GOARCH="386" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/x86/libgost-plugin.so ./cmd/gost
+go build -buildvcs=false -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/x86/libgost-plugin.so ./cmd/gost
 
 CC=$(find $ANDROID_NDK_ROOT | grep 'x86_64-linux-android21-clang$') \
 GOOS="android" GOARCH="amd64" CGO_ENABLED="1" \
-go build -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/x86_64/libgost-plugin.so ./cmd/gost
+go build -buildvcs=false -ldflags "-s -w" -a -o ../../app/src/main/jniLibs/x86_64/libgost-plugin.so ./cmd/gost
